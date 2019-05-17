5a2e3c4143a6d9b6b0b4d4a2f7075559f8d4468e
esme <esme@github.com>
2019-05-18 02:03:19 +0800

    finished problems

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,73 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(typeof(obj) === 'number') {
+    return '' + obj;
+  }
+  if(obj === null) {
+    return 'null';
+  };
+  if(typeof(obj) === 'boolean') {
+    if(obj === true) {
+      return 'true';
+    } else return 'false';
+  }
+  if(typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if(typeof obj === 'object' && obj.length === undefined) {
+    var str1 = '{'
+    var i = 0
+    for(var key in obj) {
+      if(i>0) {
+        str1 += ',';
+      }
+      if(typeof obj[key] === 'string') {
+        str1 += '"' + key + '":"' + obj[key] + '"';
+      }
+      if(typeof obj[key] === 'boolean') {
+        str1 += '"' + key + '":' + obj[key];
+      }
+      if(obj[key] === null) {
+        str1 += '"' + key + '":' + obj[key];
+      }
+      else if(typeof obj[key] === 'object') {
+        return str1 += '"' + key + '":' + stringifyJSON(obj[key]) + '}';
+      }
+      i++;
+    }
+    str1 += '}';
+    return str1;
+  }
+  else {
+    var str = '[';
+    for(var i=0; i<obj.length; i++) {
+      if(i>0) {
+        str += ',';
+      }
+      if(typeof obj[i] === 'number') {
+        str += obj[i];
+      }
+      if(typeof obj[i] === 'string') {
+        str += '"';
+        str += obj[i];
+        str += '"';
+      }
+      if(typeof obj[i] === 'object' && obj[i].length === 0 && obj[i].constructor === Array) {
+        // console.log(obj[i].constructor === Array);
+        // console.log(obj[i]);
+        // console.log('array')
+        str += '[]';
+      }
+      if(typeof obj[i] === 'object' && obj[i].length > 0) {
+        return str += stringifyJSON(obj[i]) + ']';
+      }
+      if(typeof obj[i] === 'object' && obj[i].length === undefined) {
+        console.log('object')
+        return str += stringifyJSON(obj[i]);
+      }
+    }
+    str += ']';
+    return str;
+  };
 };
